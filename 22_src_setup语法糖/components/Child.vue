9c88050fa60248032f84743c3a2d62e335d605f6
vitamin66 <13177426750@163.com>
2023-08-13 10:26:29 +0800

    vue3练习

---
+++ 22_src_setup语法糖/components/Child.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="child">
+    <h2>我是Child组件</h2>
+  </div>
+</template>
+
+<script>
+import { } from "vue";
+export default {
+  name: "MyChild",
+
+};
+</script>
+
+<style scoped>
+.child {
+  background-color: skyblue;
+  padding: 10px;
+}
+</style>
